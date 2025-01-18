65704e14321ba12594dd6391fd65d58644f27e42
Wyqmiao <157478419+Wyqmiao@users.noreply.github.com>
2025-01-18 21:27:18 +0800

    Update RoCAFENet.py

--- model/RoCAFENet.py
+++ model/RoCAFENet.py
@@ -440,7 +440,7 @@ class RoCAFENet(nn.Module):
         super(RoCAFENet, self).__init__()
 
         self.backbone = pvt_v2_b2()  # [64, 128, 320, 512]
-        path = '/home/wyq/test/model/pvt_v2_b2.pth'
+        path = './model/pvt_v2_b2.pth'
         save_model = torch.load(path)
         model_dict = self.backbone.state_dict()
         state_dict = {k: v for k, v in save_model.items() if k in model_dict.keys()}
